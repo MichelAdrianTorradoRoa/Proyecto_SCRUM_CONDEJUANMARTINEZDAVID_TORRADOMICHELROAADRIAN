4119bcb6f6216205ef8246543dc12527acb26e80
Josedavidtarazonadiaz <166555754+Josedavidtarazonadiaz@users.noreply.github.com>
2024-06-28 12:37:38 -0500

    Update FaixApp_I_S.py
    creacion y actualizacion sistema de log in

--- FaixApp_I_S.py
+++ FaixApp_I_S.py
@@ -8,8 +8,25 @@ def Sign_In_FaixApp_Artisrt(datos):
     while True:
         contraseña_verificar = input("Verificar contraseña: ")
         if contraseña_verificar == Datos_Artista["Contraseña"]:
-            Datos_Artista["D_Artista"].append(Datos_Artista)
+            datos["D_Artista"].append(Datos_Artista)
             print("Usuario Creado Exitosamente")
+            return Datos_Artista
         else:
             print("Contraseña Incorrecta")
             print("Las contraseñas no coinciden")
+
+def Sign_In_FaixApp_record_company(datos):
+    Datos_Discografica = {}
+    
+    print("Bienvenido Usuario")
+    Datos_Discografica["Nombre de la Discografica"] = input("Ingrese el nombre de la discografica: ")
+    Datos_Discografica["Contraseña"] = input("Cree un contraseña: ")
+
+    while True:
+        contraseña_Ver = input("Verificar Contraseña: ")
+        if Datos_Discografica["Contraseña"] == contraseña_Ver:
+            datos["D_Discografica"].append(Datos_Discografica)
+            print("Usuario Creado Exitosamente")
+            return Datos_Discografica
+        else:
+            print("Las Contraseñas no coinciden")
