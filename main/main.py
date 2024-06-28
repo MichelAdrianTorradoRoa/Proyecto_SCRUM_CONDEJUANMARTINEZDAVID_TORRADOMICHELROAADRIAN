223d9b7629c7b401d82a6549b62885f49157ab77
DLeonardoG <darkleonardog@gmail.com>
2024-06-28 11:12:54 -0500

    Co-authored-by: Juan David Conde Martinez <juanconde025@users.noreply.github.com>
    feat(main): añadir nueva funcionalidad al main que funciona ya como maquetacion
    
    Se ha añadido la funcionalidad del main que permite a los usuarios interactuar con los menus de manera efectiva, mas el login

--- main/main.py
+++ main/main.py
@@ -6,107 +6,166 @@ while True:
     mostrar_txt(principal)
     op = input("Seleccione una opcion:\n👉   ")
     line()
-    if op == "1": 
+    if op == "1":
         while True:
-            mostrar_txt(m_1_1)
-            op_1_1 = input("Seleccione una opcion:\n👉   ")
+            mostrar_txt(m_login_a)
+            op_login = input("Seleccione una opcion:\n👉   ")
             line()
-            if op_1_1 == "1": 
+            if op_login == "1":
+                print("login")
                 while True:
-                    mostrar_txt(m_1_1_1)
-                    op_1_1_1 = input("Seleccione una opcion:\n👉   ")
-                    if op_1_1_1 == "1":
-                        print ("crear_album")
-                    elif op_1_1_1 == "2":
-                        print ("crear_cancion")
-                    elif op_1_1_1 == "3":
-                        print ("crear_sencillo")
-                    elif op_1_1_1 == "4":
-                        mostrar_txt(m_info)
-                        op_info = input("Seleccione una opcion:\n👉   ")
-                        if op_info == "1":
-                            print ("crear_descripcion")
-                        elif op_info == "2":
-                            print ("crear_info_contacto")
-                        elif op_info == "3": break
-                        else: print ("opcion no valida")
-                    elif op_1_1_1 == "5": break
+                    mostrar_txt(m_1_1)
+                    op_1_1 = input("Seleccione una opcion:\n👉   ")
+                    line()
+                    if op_1_1 == "1": 
+                        while True:
+                            mostrar_txt(m_1_1_1)
+                            op_1_1_1 = input("Seleccione una opcion:\n👉   ")
+                            if op_1_1_1 == "1":
+                                print ("crear_album")
+                            elif op_1_1_1 == "2":
+                                print ("crear_cancion")
+                            elif op_1_1_1 == "3":
+                                print ("crear_sencillo")
+                            elif op_1_1_1 == "4":
+                                while True:
+                                    mostrar_txt(m_info)
+                                    op_info = input("Seleccione una opcion:\n👉   ")
+                                    if op_info == "1":
+                                        print ("crear_descripcion")
+                                    elif op_info == "2":
+                                        print ("crear_info_contacto")
+                                    elif op_info == "3": break
+                                    else: print ("opcion no valida")
+                            elif op_1_1_1 == "5": break
+                            else: print ("opcion no valida")
+                    elif op_1_1 == "2": 
+                        while True:
+                            mostrar_txt(m_1_1_2)
+                            op_1_1_1 = input("Seleccione una opcion:\n👉   ")
+                            if op_1_1_1 == "1":
+                                print ("eliminar_album")
+                            elif op_1_1_1 == "2":
+                                print ("eliminar_cancion")
+                            elif op_1_1_1 == "3":
+                                print ("eliminar_sencillo")
+                            elif op_1_1_1 == "4":
+                                while True:
+                                    mostrar_txt(m_info)
+                                    op_info = input("Seleccione una opcion:\n👉   ")
+                                    if op_info == "1":
+                                        print ("eliminar_descripcion")
+                                    elif op_info == "2":
+                                        print ("eliminar_info_contacto")
+                                    elif op_info == "3": break
+                                    else: print ("opcion no valida")
+                            elif op_1_1_1 == "5": break
+                            else: print ("opcion no valida")
+                    elif op_1_1 == "3":
+                        while True:
+                            mostrar_txt(m_1_1_3)
+                            op_1_1_1 = input("Seleccione una opcion:\n👉   ")
+                            if op_1_1_1 == "1":
+                                print ("actualiza_album")
+                            elif op_1_1_1 == "2":
+                                print ("actualizar_cancion")
+                            elif op_1_1_1 == "3":
+                                print ("actualizar_sencillo")
+                            elif op_1_1_1 == "4":
+                                while True:
+                                    mostrar_txt(m_info)
+                                    op_info = input("Seleccione una opcion:\n👉   ")
+                                    if op_info == "1":
+                                        print ("actualizar_descripcion")
+                                    elif op_info == "2":
+                                        print ("actualizar_info_contacto")
+                                    elif op_info == "3": break
+                                    else: print ("opcion no valida")
+                            elif op_1_1_1 == "5": break
+                            else: print ("opcion no valida")
+                    elif op_1_1 == "4": 
+                        while True:
+                            mostrar_txt(m_1_1_4)
+                            op_1_1_1 = input("Seleccione una opcion:\n👉   ")
+                            if op_1_1_1 == "1":
+                                print ("leer_albunes")
+                            elif op_1_1_1 == "2":
+                                print ("leer_canciones")
+                            elif op_1_1_1 == "3":
+                                print ("leer_sencillos")
+                            elif op_1_1_1 == "4":
+                                while True:
+                                    mostrar_txt(m_info_leer)
+                                    op_info = input("Seleccione una opcion:\n👉   ")
+                                    if op_info == "1":
+                                        print ("leer_descripcion")
+                                    elif op_info == "2":
+                                        print ("leer_info_contacto")
+                                    elif op_info == "3":
+                                        print ("leer_contrato")
+                                    elif op_info == "4": break
+                                    else: print ("opcion no valida")
+                            elif op_1_1_1 == "5": break
+                            else: print ("opcion no valida")
+                    elif op_1_1 == "5": break
                     else: print ("opcion no valida")
-            elif op_1_1 == "2": 
-                while True:
-                    mostrar_txt(m_1_1_2)
-                    op_1_1_1 = input("Seleccione una opcion:\n👉   ")
-                    if op_1_1_1 == "1":
-                        print ("eliminar_album")
-                    elif op_1_1_1 == "2":
-                        print ("eliminar_cancion")
-                    elif op_1_1_1 == "3":
-                        print ("eliminar_sencillo")
-                    elif op_1_1_1 == "4":
-                        mostrar_txt(m_info)
-                        op_info = input("Seleccione una opcion:\n👉   ")
-                        if op_info == "1":
-                            print ("eliminar_descripcion")
-                        elif op_info == "2":
-                            print ("eliminar_info_contacto")
-                        elif op_info == "3": break
-                        else: print ("opcion no valida")
-                    elif op_1_1_1 == "5": break
-                    else: print ("opcion no valida")
-            elif op_1_1 == "3":
-                while True:
-                    mostrar_txt(m_1_1_3)
-                    op_1_1_1 = input("Seleccione una opcion:\n👉   ")
-                    if op_1_1_1 == "1":
-                        print ("actualiza_album")
-                    elif op_1_1_1 == "2":
-                        print ("actualizar_cancion")
-                    elif op_1_1_1 == "3":
-                        print ("actualizar_sencillo")
-                    elif op_1_1_1 == "4":
-                        mostrar_txt(m_info)
-                        op_info = input("Seleccione una opcion:\n👉   ")
-                        if op_info == "1":
-                            print ("actualizar_descripcion")
-                        elif op_info == "2":
-                            print ("actualizar_info_contacto")
-                        elif op_info == "3": break
-                        else: print ("opcion no valida")
-                        print ("actualizar_info")
-                    elif op_1_1_1 == "5": break
-                    else: print ("opcion no valida")
-            elif op_1_1 == "4": 
-                while True:
-                    mostrar_txt(m_1_1_4)
-                    op_1_1_1 = input("Seleccione una opcion:\n👉   ")
-                    if op_1_1_1 == "1":
-                        print ("leer_albunes")
-                    elif op_1_1_1 == "2":
-                        print ("leer_canciones")
-                    elif op_1_1_1 == "3":
-                        print ("leer_sencillos")
-                    elif op_1_1_1 == "4":
-                        mostrar_txt(m_info_leer)
-                        op_info = input("Seleccione una opcion:\n👉   ")
-                        if op_info == "1":
-                            print ("leer_descripcion")
-                        elif op_info == "2":
-                            print ("leer_info_contacto")
-                        elif op_info == "3":
-                            print ("leer_contrato")
-                        elif op_info == "4": break
-                    elif op_1_1_1 == "5": break
-                    else: print ("opcion no valida")
-            elif op_1_1 == "5": break
+            elif op_login == "2":
+                print("registrar")
+            elif op_login == "3": break
             else: print ("opcion no valida")
-        
-    elif op == "2": print ("Discografia") 
-    elif op == "3": 
+    elif op == "2": 
+        print ("Discografia")
+        while True:
+            mostrar_txt(m_login_d)
+            op_login = input("Seleccione una opcion:\n👉   ")
+            line()
+            if op_login == "1":
+                print("login")
+                while True:
+                    mostrar_txt(m_1_2)
+                    op_1_2 = input("Seleccione una opcion:\n👉   ")
+                    line()
+                    if op_1_2 == "1":
+                        while True:
+                            mostrar_txt(m_1_2_1)
+                            op_1_2_1 = input("Seleccione una opcion:\n👉   ")
+                            line()
+                            if op_1_2_1 == "1":
+                                print("rock")
+                            elif op_1_2_1 == "2":
+                                print("pop")
+                            elif op_1_2_1 == "3":
+                                print("urbana")
+                            elif op_1_2_1 == "4":
+                                print("electronica")
+                            elif op_1_2_1 == "5":
+                                print("rap")
+                            elif op_1_2_1 == "6": break
+                            else: print ("opcion no valida")
+                    elif op_1_2 == "2":
+                        mostrar_txt(m_1_2_2)
+                    elif op_1_2 == "3":
+                        while True:
+                            mostrar_txt(m_1_2_3)
+                            op_1_2_3 = input("Seleccione una opcion:\n👉   ")
+                            line()
+                            if op_1_2_3 == "1":
+                                print("mostrar_artistas")
+                                mostrar_txt(m_1_2_2)
+                                print ("ver_artistas")
+                                op_1_2_3_1 = input("Seleccione una opcion:\n👉   ")
+                                print("seleccionar_artista")
+                            elif op_1_2_3 == "2":
+                                print("ver_contratos")
+                            elif op_1_2_3 == "3": break
+                            else: print ("opcion no valida")
+                    elif op_1_2 == "4": break
+                    else: print ("opcion no valida")
+            elif op_login == "2":
+                print("registrarse")
+            elif op_login == "3": break
+            else: print ("opcion no valida")
+    elif op == "3":
         print("Adios...")
         break
     else: print ("opcion no valida")
-    
-    
-
-
-
--- main/txt.py
+++ main/txt.py
@@ -22,3 +22,13 @@ m_1_1_3 = "main/txt/main_plantillas/artistas/1-1-3.txt"
 m_1_1_4 = "main/txt/main_plantillas/artistas/1-1-4.txt"
 m_info = "main/txt/main_plantillas/artistas/info.txt"
 m_info_leer = "main/txt/main_plantillas/artistas/info_leer.txt"
+
+#Menu DISCOGRAFIA
+m_1_2 = "main/txt/main_plantillas/discografia/1-2.txt"
+m_1_2_1 = "main/txt/main_plantillas/discografia/1-2-1.txt"
+m_1_2_2 = "main/txt/main_plantillas/discografia/1-2-2.txt"
+m_1_2_3 = "main/txt/main_plantillas/discografia/1-2-3.txt"
+
+#Login 
+m_login_a = "main/txt/main_plantillas/login/login_artistas.txt"
+m_login_d = "main/txt/main_plantillas/login/login_discografica.txt"
